7bc3e5819b95a3e5f148783c88ba7b33aa7ff1f0
Zach Banducci <zbducci@gmail.com>
2019-02-23 17:26:56 -0600

    Added linear gradient to homescreen and cleaned up code

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AuthSession } from 'expo';
+import { AuthSession, LinearGradient } from 'expo';
 
 const SPOTIFY_APP_ID = "5b65b983663c4e3bab1b85c4cb9bd5f7"
 const SCOPES = 'user-read-private user-read-email'
@@ -9,14 +9,13 @@ import {
   Platform,
   ScrollView,
   StyleSheet,
+  Dimensions,
   Text,
   TouchableOpacity,
-  View,
-  Button
+  View
 } from 'react-native';
-import { WebBrowser } from 'expo';
 
-import { MonoText } from '../components/StyledText';
+import { Button } from 'react-native-elements'
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
@@ -26,15 +25,22 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <View>
-          <Text>JamStack</Text>
+        <View style={styles.banner}>
+          <LinearGradient
+            colors={['#0cebeb', '#29ffc6']}
+            style={styles.gradientBackground}>
+            <Text style={styles.title}>JamStack</Text>
+          </LinearGradient>
         </View>
-        <View>
-          <Text>Quick, pass the aux!</Text>
-          <Text>Create a new room by logging in with Spotify</Text>
-          <Button 
-            title="Login with Spotify"
-            onPress={this._handleLogin}/>
+        <View style={styles.content}>
+          <Text style={styles.subTitle}>Quick, pass the aux!</Text>
+          <View>
+            <Text>Create a new room by logging in with Spotify</Text>
+            <Button 
+              title="Login with Spotify"
+              containerStyle={styles.loginButton}
+              onPress={this._handleLogin}/>
+          </View>
         </View>
       </View>
     );
@@ -52,24 +58,41 @@ export default class HomeScreen extends React.Component {
     })
     this.setState({ result })
   }
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
 
+const { height, width } = Dimensions.get('window')
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column'
   },
   loginButton: {
-
+    borderRadius: 100
   },
-
+  banner: {
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'stretch',
+  },
+  gradientBackground: { 
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 40,
+    padding: 20,
+    color: 'white'
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'space-around',
+    color: 'red'
+  },
+  textBox: {
+    alignItems: 'center'
+  },
+  subTitle: {
+    fontSize: 25
+  }
 });
